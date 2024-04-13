56fdd1e480869ca43b8ccb06fa6f4ce8f335735d
Divyakantswarnkar01 <Divyakantswarnkar@gmail.com>
2024-04-13 17:40:21 +0530

    minor changes related to twitter card

--- src/Helmet/MetaTags.jsx
+++ src/Helmet/MetaTags.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Helmet } from 'react-helmet-async';
 
-function MetaTags({ title, description, imageUrl }) {
+function MetaTags({ Title, description, imageUrl }) {
     return (
         <Helmet>
             <meta name="twitter:card" content="summary_large_image" />
-            <meta name="twitter:title" content={title} />
+            <meta name="twitter:site" content="@workhelper"/>
+            <meta name="twitter:title" content={Title} />
             <meta name="twitter:description" content={description} />
             <meta name="twitter:image" content={imageUrl} />
         </Helmet>
