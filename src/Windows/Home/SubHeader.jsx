7ef14f6ab85f7f41e71377033ebde300f81a76b6
Divyakantswarnkar01 <Divyakantswarnkar@gmail.com>
2024-04-13 11:39:37 +0530

    theme changed

--- src/Windows/Home/SubHeader.jsx
+++ src/Windows/Home/SubHeader.jsx
@@ -7,12 +7,12 @@ function SubHeader(){
     return(
         <div className='box-border relative bg-lime-500 w-full h-36'>   
         <ul className='flex flex-row absolute bottom-0 '>
-         <li className='flex row-auto'><Link className= ' font-mono justify-center box-border h-12 w-36 pl-3 pt-3 z-10 bg-lime-700 font-bold text-violet-950' to='/Home/'> Hello !!!</Link></li>
-         <li className='flex row-auto'><Link className= ' font-mono justify-center box-border h-12 w-36 pl-3 pt-3 z-10 bg-lime-700 font-bold text-violet-950' to='/Home/Recent'> Recent </Link></li>
-         <li className='flex row-auto'><Link className= ' font-mono justify-center box-border h-12 w-36 pl-3 pt-3 z-10 bg-lime-700 font-bold text-violet-950' to='/Home/NewBlogs'> New blogs</Link></li>
-         <li className='flex row-auto'><Link className= ' font-mono justify-center box-border h-12 w-36 pl-3 pt-3 z-10 bg-lime-700 font-bold text-violet-950' to='/Home/Quaries?'> Quaries?</Link></li>
-         <li className='flex row-auto'><Link className= ' font-mono justify-center box-border h-12 w-36 pl-3 pt-3 z-10 bg-lime-700 font-bold text-violet-950' to='/Home/Search'> Search </Link></li>
-         <li className='flex row-auto'><Link className= " font-mono justify-center box-border pl-3 pt-3 h-12 w-36 z-10 bg-lime-700 font-bold" to="/LogSign/Log_Sign">   Authenticate </Link> </li>
+         <li className='flex row-auto'><Link className= ' font-mono justify-center box-border h-12 w-36 pl-3 pt-3 z-10 bg-green-800 font-bold text-slate-400' to='/Home/'> Hello !!!</Link></li>
+         <li className='flex row-auto'><Link className= ' font-mono justify-center box-border h-12 w-36 pl-3 pt-3 z-10 bg-green-800 font-bold text-slate-400' to='/Home/Recent'> Recent </Link></li>
+         <li className='flex row-auto'><Link className= ' font-mono justify-center box-border h-12 w-36 pl-3 pt-3 z-10 bg-green-800 font-bold text-slate-400' to='/Home/NewBlogs'> New blogs</Link></li>
+         <li className='flex row-auto'><Link className= ' font-mono justify-center box-border h-12 w-36 pl-3 pt-3 z-10 bg-green-800 font-bold text-slate-400' to='/Home/Quaries?'> Quaries?</Link></li>
+         <li className='flex row-auto'><Link className= ' font-mono justify-center box-border h-12 w-36 pl-3 pt-3 z-10 bg-green-800 font-bold text-slate-400' to='/Home/Search'> Search </Link></li>
+         <li className='flex row-auto'><Link className= " font-mono justify-center box-border pl-3 pt-3 h-12 w-36 z-10 bg-green-800 font-bold" to="/LogSign/Log_Sign">   Authenticate </Link> </li>
          </ul> 
        </div>
     )
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,85 +16,85 @@ function Footer(){
 
 
         return(
-            <footer className="bg-black text-white" style={footerstyle}>
+            <footer className="bg-green-950 text-white" style={footerstyle}>
   <div className="container mx-auto py-8">
     <div className="grid grid-cols-1 md:grid-cols-6 gap-8">
       {/* Column 1 */}
       <div className="footer-col">
-        <h4 className="font-bold mb-2 text-slate-400">Get To Know!</h4>
+        <h4 className="font-bold mb-2 text-slate-400 text-sm">Get To Know!</h4>
         <ul>
-          <li><a href="#" className="hover:underline">About us</a></li>
-          <li><a href="#" className="hover:underline">Contact us</a></li>
-          <li><a href="#" className="hover:underline">Careers</a></li>
-          <li><a href="#" className="hover:underline">Workhelper Stories</a></li>
-          <li><a href="#" className="hover:underline">Press</a></li>
-          <li><a href="#" className="hover:underline">Organisation Information</a></li>
+          <li><a href="#" className="text-sm hover:underline">About us</a></li>
+          <li><a href="#" className="text-sm hover:underline">Contact us</a></li>
+          <li><a href="#" className="text-sm hover:underline">Careers</a></li>
+          <li><a href="#" className="text-sm hover:underline">Workhelper Stories</a></li>
+          <li><a href="#" className="text-sm hover:underline">Press</a></li>
+          <li><a href="#" className="text-sm hover:underline">Organisation Information</a></li>
           {/* Add more links if needed */}
         </ul>
       </div>
       
       {/* Column 2 */}
       <div className="footer-col">
-        <h4 className="font-bold mb-2 text-slate-400">Assistance</h4>
+        <h4 className="font-bold mb-2 text-slate-400 text-sm">Assistance</h4>
         <ul>
-          <li><a href="#" className="hover:underline">Payments</a></li>
-          <li><a href="#" className="hover:underline">Shipping</a></li>
-          <li><a href="#" className="hover:underline">Cancel and Refunds</a></li>
-          <li><a href="#" className="hover:underline">FAQ</a></li>
-          <li><a href="#" className="hover:underline">Report to us!</a></li>
+          <li><a href="#" className="text-sm hover:underline">Payments</a></li>
+          <li><a href="#" className="text-sm hover:underline">Shipping</a></li>
+          <li><a href="#" className="text-sm hover:underline">Cancel and Refunds</a></li>
+          <li><a href="#" className="text-sm hover:underline">FAQ</a></li>
+          <li><a href="#" className="text-sm hover:underline">Report to us!</a></li>
           {/* Add more links if needed */}
         </ul>
       </div>
       
       {/* Column 3 */}
       <div className="footer-col">
-        <h4 className="font-bold mb-2 text-slate-400">Sibling Companies</h4>
+        <h4 className="font-bold mb-2 text-slate-400 text-sm">Sibling Companies</h4>
         <ul>
-          <li><a href="#" className="hover:underline">Ekkhoj Technology</a></li>
-          <li><a href="#" className="hover:underline">Ekkhoj Advertising Agency</a></li>
-          <li><a href="#" className="hover:underline">Upcoming Organisations</a></li>
+          <li><a href="#" className="text-sm hover:underline">Ekkhoj Technology</a></li>
+          <li><a href="#" className="text-sm hover:underline">Ekkhoj Advertising Agency</a></li>
+          <li><a href="#" className="text-sm hover:underline">Upcoming Organisations</a></li>
           {/* Add more links if needed */}
         </ul>
       </div>
       
       {/* Column 4 */}
       <div className="footer-col">
-        <h4 className="font-bold mb-2 text-slate-400">Consumer Policies</h4>
+        <h4 className="font-bold mb-2 text-slate-400 text-sm">Consumer Policies</h4>
         <ul>
-          <li><a href="#" className="hover:underline">Terms and Conditions</a></li>
-          <li><a href="#" className="hover:underline">Security</a></li>
-          <li><a href="#" className="hover:underline">Privacy</a></li>
-          <li><a href="#" className="hover:underline">Sitemap</a></li>
-          <li><a href="#" className="hover:underline">Public Grievances</a></li>
-          <li><a href="#" className="hover:underline">Public Law Compliances</a></li>
+          <li><a href="#" className="text-sm hover:underline">Terms and Conditions</a></li>
+          <li><a href="#" className="text-sm hover:underline">Security</a></li>
+          <li><a href="#" className="text-sm hover:underline">Privacy</a></li>
+          <li><a href="#" className="text-sm hover:underline">Sitemap</a></li>
+          <li><a href="#" className="text-sm hover:underline">Public Grievances</a></li>
+          <li><a href="#" className="text-sm hover:underline">Public Law Compliances</a></li>
           {/* Add more links if needed */}
         </ul>
       </div>
 
        {/* Column 5 */}
        <div className="footer-col">
-        <h4 className="font-bold mb-2 text-slate-400 text-xl">Mail To Us!</h4>
+        <h4 className="font-bold mb-2 text-slate-400 text-md">Mail To Us!</h4>
         <ul>
-          <li><p href="#" className="text-lg font-bold text-zinc-300"> Ekkhoj Enterprise, Town Badlapur, Post Badlapur <br/> District Jaunpur Uttar Pradesh <br/> India, 222125 </p></li>
-          <li><p className="mt-2 mb-2 text-slate-400 font-bold">Social:</p></li>
-          <li><a href="#" className="hover:underline">Twitter</a></li>
-          <li><a href="#" className="hover:underline">Facebook</a></li>
-          <li><a href="#" className="hover:underline">Youtube</a></li>
-          <li><a href="#" className="hover:underline">Instagram</a></li>
+          <li><p href="#" className="text-sm text-zinc-300"> Ekkhoj Enterprise, Town Badlapur, Post Badlapur <br/> District Jaunpur Uttar Pradesh <br/> India, 222125 </p></li>
+          <li><p className="mt-2 mb-2 text-sm  text-slate-400 font-bold">Social:</p></li>
+          <li><a href="#" className=" text-sm hover:underline">Twitter</a></li>
+          <li><a href="#" className=" text-sm hover:underline">Facebook</a></li>
+          <li><a href="#" className=" text-sm hover:underline">Youtube</a></li>
+          <li><a href="#" className=" text-sm hover:underline">Instagram</a></li>
           {/* Add more links if needed */}
         </ul>
       </div>
 
        {/* Column 6 */}
        <div className="footer-col">
-        <h4 className="font-bold mb-2 text-slate-400">Make Money With Us!</h4>
+        <h4 className="font-bold mb-2 text-sm text-slate-400">Make Money With Us!</h4>
         <ul>
-          <li><a href="#" className="hover:underline">Sell with Workhelper</a></li>
-          <li><a href="#" className="hover:underline">Become Advertising Partner</a></li>
-          <li><a href="#" className="hover:underline">Affliation</a></li>
-          <li><a href="#" className="hover:underline">Get Sub-domain</a></li>
-          <li><a href="#" className="hover:underline">Get Hosting</a></li>
-          <li><a href="#" className="hover:underline">Advertise on Workhelper</a></li>
+          <li><a href="#" className="text-sm hover:underline">Sell with Workhelper</a></li>
+          <li><a href="#" className="text-sm hover:underline">Become Advertising Partner</a></li>
+          <li><a href="#" className="text-sm hover:underline">Affliation</a></li>
+          <li><a href="#" className="text-sm hover:underline">Get Sub-domain</a></li>
+          <li><a href="#" className="text-sm hover:underline">Get Hosting</a></li>
+          <li><a href="#" className="text-sm hover:underline">Advertise on Workhelper</a></li>
           {/* Add more links if needed */}
         </ul>
       </div>
