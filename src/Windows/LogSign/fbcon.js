e7f644abf450d8b384b80a0f7f38f5fe9eb8e0c9
Divyakant Swarnkar <162860058+DivyakantSwarnkar01@users.noreply.github.com>
2025-01-31 12:35:48 +0530

    Update fbcon.js
    removed firebase auths

--- src/Windows/LogSign/fbcon.js
+++ src/Windows/LogSign/fbcon.js
@@ -6,14 +6,7 @@ import { getStorage } from 'firebase/storage';
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAw7s53wOvlN_HKZDOc_5CiiDHDKwd5DJI",
-    authDomain: "workhelper-261bf.firebaseapp.com",
-    projectId: "workhelper-261bf",
-    storageBucket: "workhelper-261bf.appspot.com",
-    messagingSenderId: "359079196777",
-    appId: "1:359079196777:web:c0f8603146e3e0ec724af8",
-    measurementId: "G-YYEWCSKBRM",
-    databaseURL: "https://workhelper-261bf-default-rtdb.asia-southeast1.firebasedatabase.app"
+  
   };
 
 
